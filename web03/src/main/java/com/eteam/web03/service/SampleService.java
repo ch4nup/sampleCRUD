3a0d4f6f694bc746bb1d29e061ffd9cec15aa631
Administrator <Administrator@smart20105>
2019-02-08 12:31:50 +0900

    [박찬업]SampleController.java파일 생성 및 SampleService.java파일 생성 -외형만 작성

---
+++ web03/src/main/java/com/eteam/web03/service/SampleService.java
@@ -0,0 +1,35 @@
+package com.eteam.web03.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.eteam.web03.mapper.SampleMapper;
+import com.eteam.web03.vo.Sample;
+
+@Service
+public class SampleService {
+	@Autowired 
+	private SampleMapper sampleMapper;
+	
+	public List<Sample> getSampleList() {
+		return null;
+	}
+	
+	public List<Sample> getSampleOne() {
+		return null;
+	}
+	
+	public int addSample(Sample sample) {
+		return 0;
+	}
+	
+	public int removeSample(Sample sample) {
+		return 0;
+	}
+	
+	public int modifySample(Sample sample) {
+		return 0;
+	}
+}
